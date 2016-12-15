8caba16cf14a889ed10bac9176e4dd2e014d2d10
Wippermann <pwippermann@polaris.senacor.com>
2016-12-15 20:42:21 +0100

    added two independent tests

---
+++ src/test/java/com/github/peterwippermann/selenium/reuse/GoToJavaLangTest.java
@@ -0,0 +1,29 @@
+package com.github.peterwippermann.selenium.reuse;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class GoToJavaLangTest {
+
+	@Test
+	public void test() throws InterruptedException {
+		DesiredCapabilities cap = DesiredCapabilities.firefox();
+		cap.setCapability("marionette", true);
+		WebDriver driver = new FirefoxDriver(cap);
+		driver.navigate().to("https://docs.oracle.com/javase/7/docs/api/overview-summary.html");
+
+		// click 
+		WebElement javaIo = driver.findElement(By.linkText("java.lang"));
+		javaIo.click();
+
+		Thread.sleep(5000);
+
+		driver.quit();
+
+	}
+
+}
