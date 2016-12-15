b0e95d8ce873cd3c8f01adfe89359ff373e6de26
Wippermann <pwippermann@polaris.senacor.com>
2016-12-15 20:47:17 +0100

    added a test suite

---
+++ src/test/java/com/github/peterwippermann/selenium/reuse/JavaDocSuite.java
@@ -0,0 +1,10 @@
+package com.github.peterwippermann.selenium.reuse;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({GoToJavaIOTest.class, GoToJavaLangTest.class})
+public class JavaDocSuite {
+}
